d4a26332753dc9b611cbd0646cbeb546c5bdfae7
a.grebnev <artemgrebnev@gmail.com>
2022-07-07 18:24:33 +0500

    start working on project 10

--- src/app/film-info/film-info.component.ts
+++ src/app/film-info/film-info.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {GetDataService} from "../get-data.service";
 import {ActivatedRoute, Params} from "@angular/router";
-import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-film-info',
@@ -30,16 +30,26 @@ export class FilmInfoComponent implements OnInit {
 
 
         this.filmInfo = value
-        console.log(this.filmInfo)
 
         this.filmForm = new FormGroup({
           name: new FormControl(this.filmInfo.name),
           rating: new FormControl(this.filmInfo.rating),
           year: new FormControl(this.filmInfo.year),
           description: new FormControl(this.filmInfo.description),
-          awardsCheckbox: new FormControl()
+          awardsCheckbox: new FormControl(false),
+          awards: new FormArray([
+
+          ])
         })
 
+        console.log(this.filmInfo)
+        for (let i = 1; i <= this.filmInfo.awards.length; i++) {
+
+          let awardName : string = this.filmInfo.awards[i]
+          console.log(awardName)
+
+          //(<FormArray> this.filmForm.controls['awards']).push()
+        }
       })
     })
   }
